c6d13efebf915e653074a3902261133bbee6a748
Ollie <olliejonas@gmail.com>
2023-02-24 00:41:34 +0000

    stuff

--- src/main/java/me/olliejonas/saltmarsh/Main.java
+++ src/main/java/me/olliejonas/saltmarsh/Main.java
@@ -5,7 +5,7 @@ import javax.security.auth.login.LoginException;
 public class Main {
 
     public static void main(String[] args) {
-        String token = args.length > 0 ? args[0] : System.getenv("SALTMARSH_DISCORD_TOKEN");
+        String token = args.length > 0 ? args[0] : System.getenv("SALTMARSH_DISCORD_TOKEN").strip();
         System.out.println(token);
         Saltmarsh saltmarsh = new Saltmarsh(token);
 
